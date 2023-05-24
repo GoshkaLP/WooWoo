4c440e3c28b9300dccc90e7ecb77870e64724998
GoshkaLP <rybkin.georgy@yandex.ru>
2023-05-24 12:08:35 +0300

    fix config, db connection config and docker-compose

--- backend/models/db.py
+++ backend/models/db.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from config import POSTGRES_DB, POSTGRES_PASSWORD, POSTGRES_USER, HOST
+from config import POSTGRES_DB, POSTGRES_PASSWORD, POSTGRES_USER, HOST, POSTGRES_PORT
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{HOST}:5435/{POSTGRES_DB}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
